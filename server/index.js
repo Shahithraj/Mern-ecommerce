d07a02e7b7aef8fc9b1876a72904632ff86339d0
Shahith R <shahithr99@gmail.com>
2022-08-28 22:50:40 +0530

    deploy commit

--- server/index.js
+++ server/index.js
@@ -40,6 +40,7 @@ app.get('/api/keys/paypal',(req,res) => {
   app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/client/build/index.html'))
 );
+
 app.get('/', (req, res) => {
   res.send('Hello from Ecommerce');
 });
